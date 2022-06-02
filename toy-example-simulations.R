b2e2d2f53be370f15395fc7adfecf536556f451d
bcolnet <benedicte.colnet@gmail.com>
2022-06-02 14:37:07 -0700

    adapt n and m for more granularity

--- toy-example-simulations.R
+++ toy-example-simulations.R
@@ -12,8 +12,8 @@ toy.examples.varying.n.m <- data.frame("n" = c(),
                                        "estimator" = c())
 
 
-for ( neff in seq(50, 1000, by = 50)){
-  for ( meff in seq(50, 1000, by = 50)){
+for ( neff in seq(50, 500, by = 5)){
+  for ( meff in seq(10, 300, by = 5)){
     
     print(paste0("Starting n = ", neff, " and m = ", meff))
     
