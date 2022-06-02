fc6b3ba405dbaec677b9e7fa018ec76611460efe
bcolnet <benedicte.colnet@gmail.com>
2022-06-02 10:59:09 -0700

    estimator

---
+++ estimator.R
@@ -0,0 +1,105 @@
+difference.in.means <- function(dataframe, oracle.e.too = TRUE, estimand = "ATE"){
+  rct <- dataframe[dataframe$S == 1,]
+  Y = rct$Y
+  A = rct$A
+  
+  if (oracle.e.too){
+    e = rct[1, "e"]
+  } else {
+    e = mean(rct$A)
+  }
+  
+  # estimation
+  if (estimand == "ATE"){
+    estimate = mean(Y * (A/e - (1-A)/(1-e)))
+  } else if (estimand == "Y1") {
+    estimate = mean(Y *A/e)
+  }  else if (estimand == "Y0") {
+    estimate = mean(Y *(1-A)/(1-e))
+  } else {
+    print("Error in estimand, should be ATE, Y1, or Y0")
+    break
+  }
+  
+  return(estimate)
+}
+
+
+# ipsw which estimate the ratio estimator with univariate and categorical X
+ipsw.univariate.and.categorical.X <- function(dataframe, estimand = "ATE", oracle.e = F, oracle.pt = F, oracle.pr = F){
+  
+  # extract RCT and relevant quantities
+  rct <- dataframe[dataframe$S == 1,]
+  Y = rct$Y
+  A = rct$A
+  X = rct$X
+  
+  if (oracle.e){
+    e.hat.X1 = 0.5
+    e.hat.X0 = 0.5
+    
+  } else {
+    e.hat.X1 = mean(rct[rct$X == 1, "A"])
+    e.hat.X0 = mean(rct[rct$X == 0, "A"])
+  }
+  
+  
+  e.hat <- ifelse(rct$X == 1, e.hat.X1, e.hat.X0)
+  
+  if (!oracle.pt & !oracle.pr){
+    
+    r.X.1 <- mean(dataframe[dataframe$S == 0, "X"]) / mean(dataframe[dataframe$S == 1, "X"])
+    r.X.0 <- (1- mean(dataframe[dataframe$S == 0, "X"]))/ (1-mean(dataframe[dataframe$S == 1, "X"]))
+    r.X <- ifelse(rct$X == 1, r.X.1, r.X.0)
+    
+  } else if (!oracle.pt & oracle.pr){
+    
+    if(!("pr" %in% colnames(dataframe)) ) {
+      print("Error: should provide a data set with oracles quantities to proceed with option 'oracle.r'.")
+      break
+    }
+    
+    r.X.1 <- mean(dataframe[dataframe$S == 0, "X"]) / rct[rct$X == 1, "pr"][[1]]
+    r.X.0 <- (1- mean(dataframe[dataframe$S == 0, "X"]))/   rct[rct$X == 0, "pr"][[1]]
+    r.X <- ifelse(rct$X == 1, r.X.1, r.X.0)
+    
+  } else if  (oracle.pt & !oracle.pr){
+    
+    if(!("pt" %in% colnames(dataframe))) {
+      print("Error: should provide a data set with oracles quantities to proceed with option 'oracle.r'.")
+      break
+    }
+    
+    r.X.1 <- rct[rct$X == 1, "pt"][[1]] / mean(dataframe[dataframe$S == 1, "X"])
+    r.X.0 <-  rct[rct$X == 0, "pt"][[1]] / (1-mean(dataframe[dataframe$S == 1, "X"]))
+    r.X <- ifelse(rct$X == 1, r.X.1, r.X.0)
+    
+  } else { # full oracle
+    
+    if(!("pt" %in% colnames(dataframe)) | !("pr" %in% colnames(dataframe)) ) {
+      print("Error: should provide a data set with oracles quantities to proceed with option 'oracle.r'.")
+      break
+    }
+    
+    r.X <- rct$pt /  rct$pr
+    
+  }
+  
+  mean.Y1 = mean(A*Y*r.X/e.hat)
+  mean.Y0 = mean((1-A)*Y*r.X/(1-e.hat))
+  
+  # estimation
+  if (estimand == "ATE"){
+    estimate = mean.Y1 - mean.Y0
+  } else if (estimand == "Y1") {
+    estimate = mean.Y1
+  }  else if (estimand == "Y0") {
+    estimate = mean.Y0
+  } else {
+    print("Error in estimand, should be ATE, Y1, or Y0")
+    break
+  }
+  
+  
+  return(estimate)
+}
