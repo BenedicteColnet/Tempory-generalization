f10e8acf1b2706b0d1d10e24ee33205c83be9c7e
bcolnet <benedicte.colnet@gmail.com>
2022-06-29 13:38:48 +0200

    change with FALSE

--- generateDGPs.R
+++ generateDGPs.R
@@ -87,16 +87,14 @@ toy.example <- function(n = 1000, m = 1000,
   return(output)
 }
 
-
 # this is an extension of the toy example with multiple covariates
 simulation.multivariate.categorical.X <- function(n = 1000, m = 1000, 
                                                   ratio = 0.5, 
                                                   output.oracles = TRUE,
                                                   prop.X1.RCT = 0.75,
                                                   prop.X1.Target = 0.3,
-                                                  prop.X2 = 0.5,
-                                                  prop.X3.RCT = 0.1,
-                                                  prop.X3.Target = 0.9){
+                                                  prop.X2.RCT = 0.5,
+                                                  prop.X2.Target = 0.5){
   
   
   
@@ -104,23 +102,14 @@ simulation.multivariate.categorical.X <- function(n = 1000, m = 1000,
   X1.RCT <- rbinom(n = n, 1, prop.X1.RCT)
   X1.obs <- rbinom(n = m, 1, prop.X1.Target)
   
-  # X2 -- the covariate explaining nothing and not shifted
-  X2.RCT <- rbinom(n = n, 1, prop.X2)
-  X2.obs <- rbinom(n = m, 1, prop.X2)
+  # X2 -- the covariate explaining nothing and shifted
+  X2.RCT <- rbinom(n = n, 1, prop.X2.RCT)
+  X2.obs <- rbinom(n = m, 1, prop.X2.Target)
+  
+  # X3 -- the covariate explaining Y but not shifted
+  X3.RCT <- rbinom(n = n, 1, 0.2)
+  X3.obs <- rbinom(n = m, 1, 0.2)
 
-  # X3 -- the covariate explaining nothing but shifted between the two sources
-  X3.RCT <- rbinom(n = n, 1, prop.X3.RCT)
-  X3.obs <- rbinom(n = m, 1, prop.X3.Target)
-  
-  # # X3 -- the covariate explaining Y but not shifted
-  # X3.RCT <- rbinom(n = n, 1, 0.2)
-  # X3.obs <- rbinom(n = m, 1, 0.2)
-  # 
-  
-  # 
-  # # X5 -- the covariate explaining Y and shifted
-  # X5.RCT <- rbinom(n = n, 1, 0.2)
-  # X5.obs <- rbinom(n = m, 1, 0.8)
   
   # random treatment assignment within the RCT / Bernoulli trial
   treat.assignment.in.RCT <-  rbinom(n, 1, ratio)
@@ -139,49 +128,17 @@ simulation.multivariate.categorical.X <- function(n = 1000, m = 1000,
   error_0_X3 = rnorm(n = nrow(output), mean = 0, sd = 1)
   error_1_X3 = rnorm(n = nrow(output), mean = 0, sd = 1)
   
-  # Additional error due to X5
-  error_0_X5 = rnorm(n = nrow(output), mean = 0, sd = 1)
-  error_1_X5 = rnorm(n = nrow(output), mean = 0, sd = 1)
-  
-  
-  # Y_0 <- ifelse(output$X1 == 1, 0, 0) + ifelse(output$X3 == 1, 100 + error_0_X3, error_0_X3) + ifelse(output$X5 == 1, -100 + error_0_X5, error_0_X5)
-  # output$Y_0 <- Y_0 + error_0
-  # Y_1 <- ifelse(output$X1 == 1, 10, 5) +  ifelse(output$X3 == 1, 100 + error_1_X3 , error_1_X3) + ifelse(output$X5 == 1, -100 + error_1_X5, error_1_X5)
-  # output$Y_1 <- Y_1 + error_1
-  
-  Y_0 <- ifelse(output$X1 == 1, 0, 0)
+  Y_0 <- ifelse(output$X1 == 1, 0, 0) + ifelse(output$X3 == 1, 100 + error_0_X3, error_0_X3) 
   output$Y_0 <- Y_0 + error_0
-  Y_1 <- ifelse(output$X1 == 1, 10, 5)
+  Y_1 <- ifelse(output$X1 == 1, 10, 5) +  ifelse(output$X3 == 1, 100 + error_1_X3 , error_1_X3)
   output$Y_1 <- Y_1 + error_1
 
-  
   # observed outcome
   output$Y <- ifelse(output$S == 1, ifelse(output$A == 1, output$Y_1, output$Y_0), NA)
   
-  if(output.oracles){
-    
-    # add oracle quantities
-    output$e <- ifelse(output$S == 1, ratio, NA)
-    
-    output$pt <- case_when(output$X1 == 1 & output$X3 == 1 ~ prop.X1.Target*prop.X3.Target,
-                           output$X1 == 1 & output$X3 == 0 ~ prop.X1.Target*(1-prop.X3.Target),
-                           output$X1 == 0 & output$X3 == 1 ~ (1-prop.X1.Target)*prop.X3.Target,
-                           output$X1 == 0 & output$X3 == 0 ~ (1-prop.X1.Target)*(1-prop.X3.Target))
-
-    output$pr <- case_when(output$X1 == 1 & output$X3 == 1 ~ prop.X1.RCT*prop.X3.RCT,
-                           output$X1 == 1 & output$X3 == 0 ~ prop.X1.RCT*(1-prop.X3.RCT),
-                           output$X1 == 0 & output$X3 == 1 ~ (1-prop.X1.RCT)*prop.X3.RCT,
-                           output$X1 == 0 & output$X3 == 0 ~ (1-prop.X1.RCT)*(1-prop.X3.RCT))
-
-      output$pt <- ifelse(output$S == 1, output$pt, NA)
-      output$pr <- ifelse(output$S == 1, output$pr, NA)
-    
-    
-    
-  } else {
-    # keep only the interesting subset of covariates
+  if(!output.oracles){
     output <- output[, c("X1", "X2", "X3", "A", "Y", "S")]
-  }
+  } 
   
   return(output)
 }
@@ -200,8 +157,8 @@ simulation.semi.synthetic <- function(n = 1000, m = 1000, ratio = 0.5, output.or
   source.Obs <- total.with.overlap[total.with.overlap$S == 0,]
   
   if(!generate.associated.ground.truth){
-    RCT <- source.RCT[sample(1:nrow(source.RCT), n, replace = TRUE), ]
-    Obs <- source.Obs[sample(1:nrow(source.Obs), m, replace = TRUE), ]
+    RCT <- source.RCT[sample(1:nrow(source.RCT), n, replace = FALSE), ]
+    Obs <- source.Obs[sample(1:nrow(source.Obs), m, replace = FALSE), ]
   } else {
     RCT <- source.RCT
     Obs <- source.Obs
