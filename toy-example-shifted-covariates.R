37ad558146ee0c871abd45ec061bf18455ed3116
bcolnet <benedicte.colnet@gmail.com>
2022-07-04 17:54:31 +0200

    script for toy example and shifted covariate

---
+++ toy-example-shifted-covariates.R
@@ -0,0 +1,31 @@
+set.seed(123)
+
+library(MASS)
+
+source("./estimators.R")
+source("./generateDGPs.R")
+options(dplyr.summarise.inform = FALSE)
+
+shifted.covariates <- data.frame("estimate" = c(),
+                                 "pt" = c(), 
+                                 "covariate.set" =  c())
+
+for (pteff in seq(0.05, 0.5, by = 0.025)){
+  for (i in 1:500){
+    
+    simulation <- simulation.multivariate.categorical.X(n = 1000, m = 20000, prop.X2.Target = pteff)
+    
+    extended <- ipsw.binned(simulation, covariates_names_vector = c("X1", "X2"), oracle.e = F, oracle.pt = F, oracle.pr = F)
+    minimal <- ipsw.binned(simulation, covariates_names_vector = c("X1"), oracle.e = F, oracle.pt = F, oracle.pr = F)
+    
+    new.row <- data.frame("estimate" = c(extended, minimal),
+                          "pt" = c(pteff, pteff),
+                          "covariate.set" = c("Extended", "Minimal"))
+    
+    shifted.covariates <- rbind(shifted.covariates, new.row)
+    
+  }  
+}
+
+write.csv(shifted.covariates, "./results/toy.example.shifted.covariates.csv", row.names = TRUE)
+
