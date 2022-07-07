2b9a26924a7371bc310bcb8230a07ad5c1d518b2
bcolnet <benedicte.colnet@gmail.com>
2022-07-07 16:42:06 +0200

    more samples in RCT

--- toy-example-shifted-covariates.R
+++ toy-example-shifted-covariates.R
@@ -16,7 +16,7 @@ shifted.covariates <- data.frame("estimate" = c(),
 for (pteff in seq(0.05, 0.5, by = 0.1)){
   print(pteff)
   
-  for (neff in c(5000)){
+  for (neff in c(10000)){
       for (i in 1:100){
         
         simulation <- simulation.multivariate.categorical.X(n = neff, m = 50000, prop.X2.Target = pteff)
