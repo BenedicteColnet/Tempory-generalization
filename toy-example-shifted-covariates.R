d93b21bb319fa17f26f112631ae4ace4508ea799
bcolnet <benedicte.colnet@gmail.com>
2022-07-19 10:40:31 +0200

    simulation to show the problem

--- toy-example-shifted-covariates.R
+++ toy-example-shifted-covariates.R
@@ -13,25 +13,36 @@ shifted.covariates <- data.frame("estimate" = c(),
                                  "covariate.set" =  c(),
                                  "n" = c())
 
-for (pteff in seq(0.05, 0.5, by = 0.05)){
+for (pteff in seq(0.1, 0.5, by = 0.05)){
   print(pteff)
   
-  for (neff in c(1000, 5000, 10000, 20000)){
-      for (i in 1:1000){
+  for (neff in c(100, 500, 1000)){
+    for (meff in c(100, 500, 1000)){
+      for (i in 1:500){
         
-        simulation <- simulation.multivariate.categorical.X(n = neff, m = 50000, prop.X2.Target = pteff)
+        simulation <- toy.example(n = neff, m = meff, output.oracles = F, symetric.po = F, noisier.var.X1 = F)
         
-        extended <- ipsw.binned(simulation, covariates_names_vector = c("X1", "X2"), oracle.e = F, oracle.pt = F, oracle.pr = F)
-        minimal <- ipsw.binned(simulation, covariates_names_vector = c("X1"), oracle.e = F, oracle.pt = F, oracle.pr = F)
+        # useless covariates
+        X.shifted.obs <- rbinom(n = meff, 1, pteff)
+        X.shifted.trial <- rbinom(n = neff, 1, 0.5)
+        
+        
+        useless.covariates <- c(X.shifted.trial, X.shifted.obs)
+        simulation$X.supp <- useless.covariates
+        
+        extended <- ipsw.binned(simulation, covariates_names_vector = c("X", "X.supp"), oracle.e = F, oracle.pt = F, oracle.pr = F)
+        minimal <- ipsw.binned(simulation, covariates_names_vector = c("X"), oracle.e = F, oracle.pt = F, oracle.pr = F)
         
         new.row <- data.frame("estimate" = c(extended, minimal),
                               "pt" = c(pteff, pteff),
                               "covariate.set" = c("Extended", "Minimal"),
-                              "n" = c(neff, neff))
+                              "n" = c(neff, neff),
+                              "m" =  c(meff, meff))
         
         shifted.covariates <- rbind(shifted.covariates, new.row)
         
       } 
+    }
   }
   
 }
