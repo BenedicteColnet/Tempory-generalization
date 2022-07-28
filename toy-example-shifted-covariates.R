404b8d6dd5959a8e7afbbebdab2bebb76bf28e7d
bcolnet <benedicte.colnet@gmail.com>
2022-07-28 17:13:57 +0200

    complete the toy example shifted

--- toy-example-shifted-covariates.R
+++ toy-example-shifted-covariates.R
@@ -13,33 +13,36 @@ shifted.covariates <- data.frame("estimate" = c(),
                                  "covariate.set" =  c(),
                                  "n" = c())
 
-for (pteff in seq(0.1, 0.5, by = 0.05)){
+for (pteff in seq(0.1, 0.9, by = 0.05)){
   print(pteff)
   
   for (neff in c(250, 500, 1000)){
-    for (meff in c(1000)){
-      for (i in 1:10000){
-        
-        simulation <- toy.example(n = neff, m = meff, output.oracles = F, symetric.po = F, noisier.var.X1 = F)
-        
-        # useless covariates
-        X.shifted.obs <- rbinom(n = meff, 1, pteff)
-        X.shifted.trial <- rbinom(n = neff, 1, 0.5)
-        
-        
-        useless.covariates <- c(X.shifted.trial, X.shifted.obs)
-        simulation$X.supp <- useless.covariates
-        
-        extended <- ipsw.binned(simulation, covariates_names_vector = c("X", "X.supp"), oracle.e = F, oracle.pt = F, oracle.pr = F)
-        minimal <- ipsw.binned(simulation, covariates_names_vector = c("X"), oracle.e = F, oracle.pt = F, oracle.pr = F)
-        
-        new.row <- data.frame("estimate" = c(extended, minimal),
-                              "pt" = c(pteff, pteff),
-                              "covariate.set" = c("Extended", "Minimal"),
-                              "n" = c(neff, neff),
-                              "m" =  c(meff, meff))
-        
-        shifted.covariates <- rbind(shifted.covariates, new.row)
+    for (meff in c(5000, 50000)){
+      for (i in 1:100){
+        for (preff in c(0.5, 0.25, 0.1)){
+          
+            # generate data
+            simulation <- toy.example(n = neff, m = meff, output.oracles = F, symetric.po = F, noisier.var.X1 = F)
+            
+            # useless covariates
+            X.shifted.obs <- rbinom(n = meff, 1, pteff)
+            X.shifted.trial <- rbinom(n = neff, 1, preff)
+            
+            
+            useless.covariates <- c(X.shifted.trial, X.shifted.obs)
+            simulation$X.supp <- useless.covariates
+            
+            extended <- ipsw.binned(simulation, covariates_names_vector = c("X", "X.supp"), oracle.e = T, oracle.pt = F, oracle.pr = F)
+            minimal <- ipsw.binned(simulation, covariates_names_vector = c("X"), oracle.e = T, oracle.pt = F, oracle.pr = F)
+            
+            new.row <- data.frame("estimate" = c(extended, minimal),
+                                  "pt" = c(pteff, pteff),
+                                  "covariate.set" = c("Extended", "Minimal"),
+                                  "n" = c(neff, neff),
+                                  "m" =  c(meff, meff))
+            
+            shifted.covariates <- rbind(shifted.covariates, new.row)
+        }
         
       } 
     }
