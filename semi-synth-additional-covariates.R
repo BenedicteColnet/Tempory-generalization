2866943e3f92114612f3112b67a048a85d25a77b
bcolnet <benedicte.colnet@gmail.com>
2022-08-02 16:48:37 +0200

    add 2 scripts for semi synthetic

---
+++ semi-synth-additional-covariates.R
@@ -0,0 +1,43 @@
+source("./estimators.R")
+source("./generateDGPs.R")
+
+
+set.seed(123)
+options(dplyr.summarise.inform = FALSE)
+
+# libraries
+library(tidyr) # pivot longer or wider
+library(dplyr) # case_when and others
+
+# Load all data necessary for semi-synthetic simulation
+load("./data/semi-synthetic-DGP.rds")
+load("./data/semi-synthetic-oracle-target.rds")
+load("./data/semi-synthetic-oracle-trial.rds")
+
+# covariates needed
+MINIMAL_SET <- c("time_to_treatment.categorized",  "Glasgow.initial")
+EXTENDED_PRECISION_SET <- c("time_to_treatment.categorized",  "Glasgow.initial", "gender", "systolicBloodPressure.categorized")
+EXTENDED_SHIFTED_SET <- c("time_to_treatment.categorized",  "Glasgow.initial", "pupilReact_num")
+additional.covariates.shifted <- data.frame("estimate" = c(),
+                                            "covariate.set" = c())
+
+for (i in 1:1000){
+  
+  print(i)
+  
+  simulation <- simulation.semi.synthetic(n = 500, m = 10000, source.data = total.with.overlap)
+  
+  ipsw.minimal <- ipsw.binned(dataframe = simulation, covariates_names_vector = MINIMAL_SET, oracle.e = F, oracle.pt = F, oracle.pr = F)
+  
+  ipsw.precision <- ipsw.binned(dataframe = simulation, covariates_names_vector = EXTENDED_PRECISION_SET, oracle.e = F, oracle.pt = F, oracle.pr = F)
+  
+  ipsw.shifted <- ipsw.binned(dataframe = simulation, covariates_names_vector = EXTENDED_SHIFTED_SET, oracle.e = F, oracle.pt = F, oracle.pr = F)
+  
+  new.row <- data.frame("estimate" = c(ipsw.minimal, ipsw.precision, ipsw.shifted),
+                        "covariate.set" = c("Minimal", "Extended - Precision", 'Extended shifted'))
+  
+  additional.covariates.shifted <- rbind(additional.covariates.shifted, new.row)
+  
+}
+
+write.csv(additional.covariates.shifted, "./results/semi.synthetic.shifted.csv", row.names = TRUE)
---
+++ semi-synthetic-varying.n.m.R
@@ -0,0 +1,66 @@
+set.seed(123)
+
+library(MASS)
+library(tidyr) # pivot longer or wider
+library(dplyr) # case_when and others
+
+options(dplyr.summarise.inform = FALSE)
+
+source("./estimators.R")
+source("./generateDGPs.R")
+
+
+semi.synthetic.simulations <- data.frame("estimate" = c(),
+                                        "n" = c(),
+                                        "m" = c(),
+                                        "method" = c())
+
+# Prepare oracles
+source.data <- load("./data/semi-synthetic-DGP.rds")
+source.RCT <- total.with.overlap[total.with.overlap$S == 1,]
+source.Obs <- total.with.overlap[total.with.overlap$S == 0,]
+
+
+oracle.proba.trial <- source.RCT %>% 
+  group_by(time_to_treatment.categorized, Glasgow.initial) %>%
+  summarise(pr = n()/nrow(source.RCT))
+
+oracle.proba.target <- source.Obs %>% 
+  group_by(time_to_treatment.categorized, Glasgow.initial) %>%
+  summarise(pt = n()/nrow(source.Obs))
+
+
+for (neff in seq(500, 3000, by = 500)){
+  
+  for (meff in seq(3000, 10000, by = 1000)){
+    
+    print(paste0("Starting n = ", neff, " and m = ", meff))
+    
+    for (i in 1:500){ 
+      
+      simulation <- simulation.semi.synthetic(n = neff, m = meff, output.oracles = TRUE, extra.noise.on.high.ttt = F)
+      
+      # add oracles
+      simulation <- merge(simulation, oracle.proba.target)
+      simulation <- merge(simulation, oracle.proba.trial)
+      
+      oracle <- ipsw.binned(simulation, covariates_names_vector = c("time_to_treatment.categorized", "Glasgow.initial"), oracle.e = T, oracle.pt = T, oracle.pr = T)
+      
+      semi.oracle <- ipsw.binned(simulation, covariates_names_vector = c("time_to_treatment.categorized", "Glasgow.initial"), oracle.e = T, oracle.pt = T, oracle.pr = F)
+      
+      ipsw.estimate <- ipsw.binned(simulation, covariates_names_vector = c("time_to_treatment.categorized", "Glasgow.initial"), oracle.e = T, oracle.pt = F, oracle.pr = F)
+      
+      naive.estimate <- difference.in.means(simulation)
+      
+      new.row <- data.frame("estimate" = c(oracle, semi.oracle, ipsw.estimate, naive.estimate),
+                            "n" = rep(neff,4),
+                            "m" = rep(meff, 4),
+                            "method" = c("oracle", "semi-oracle", "ipsw", "naive"))
+      semi.synthetic.simulations <- rbind(semi.synthetic.simulations, new.row)
+      
+    }
+  }
+}
+
+
+write.csv(semi.synthetic.simulations, "./results/semisynthetic.varying.n.m.csv", row.names = TRUE)
