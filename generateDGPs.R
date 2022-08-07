1d2421a74d787e995acdbf1858d8521b1d641d97
bcolnet <benedicte.colnet@gmail.com>
2022-08-07 10:40:54 +0200

    on converge git add semi-synth-additional-covariates.R

--- generateDGPs.R
+++ generateDGPs.R
@@ -161,7 +161,7 @@ simulation.semi.synthetic <- function(n = 1000, m = 1000, ratio = 0.5, output.or
   baseline <- (10 - total$Glasgow.initial) 
   - 1*(total$systolicBloodPressure.categorized-1)^2
   
-  cate <- ifelse(total$age.categorized == 1, 9, -3*total$age.categorized) + 15*(6-total$time_to_treatment.categorized) 
+  cate <- ifelse(total$age.categorized == 1, 4, -3*total$age.categorized) + 15*(6-total$time_to_treatment.categorized) + 3*total$gender
   
   total$Y_0 = baseline 
   total$Y_1 =  baseline + cate
