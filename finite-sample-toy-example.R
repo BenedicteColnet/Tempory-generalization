c8d1d482899354d5f29b6843a535b017088efee0
bcolnet <benedicte.colnet@gmail.com>
2022-08-10 15:18:24 +0200

    new script

---
+++ finite-sample-toy-example.R
@@ -0,0 +1,96 @@
+# libraries
+library(MASS) # simulations
+library(ggplot2) # plots
+library(tidyr) # pivot longer or wider
+library(dplyr) # case_when and others
+
+source("./estimators.R")
+source("./generateDGPs.R")
+
+options(dplyr.summarise.inform = FALSE)
+
+# Load all data necessary for semi-synthetic simulation
+load("./data/semi-synthetic-DGP.rds")
+load("./data/semi-synthetic-oracle-target.rds")
+load("./data/semi-synthetic-oracle-trial.rds")
+
+finite.sample.semi.oracle <- data.frame("estimate" = c(),
+                                        "method" = c(),
+                                        "n" = c(),
+                                        "m" = c())
+
+for (i in 1:2000){
+  
+  print(i)
+  
+  for (neff in c(500, 1000, 1500)){
+    
+    # Generate data for oracle and semi oracle
+    simulation <- toy.example(n = neff, m = 20, output.oracles = T, noisier.var.X1 = F)
+    
+    # fully oracle
+    ipsw <- ipsw.univariate.and.categorical.X(dataframe = simulation, oracle.e = T, oracle.pt = T, oracle.pr = T)
+    method = "oracle"
+    
+    new.row <- data.frame("estimate" = ipsw,
+                          "method" = method,
+                          "n" = neff,
+                          "m" = NA)
+    
+    finite.sample.semi.oracle <- rbind(finite.sample.semi.oracle, new.row)
+    
+    # semi oracle
+    ipsw <-  ipsw.univariate.and.categorical.X(dataframe = simulation, oracle.e = T, oracle.pt = T, oracle.pr = F)
+    method = "semi.oracle"
+    
+    new.row <- data.frame("estimate" = ipsw,
+                          "method" = method,
+                          "n" = neff,
+                          "m" = NA)
+    
+    finite.sample.semi.oracle <- rbind(finite.sample.semi.oracle, new.row)
+    
+    
+    # n = m
+    simulation <- toy.example(n = neff, m = neff, output.oracles = T, noisier.var.X1 = F)
+    ipsw <- ipsw.univariate.and.categorical.X(dataframe = simulation, oracle.e = T, oracle.pt = F, oracle.pr = F)
+    method = "ipsw - m = n"
+
+    new.row <- data.frame("estimate" = ipsw,
+                            "method" = method,
+                            "n" = neff,
+                            "m" = neff)
+      
+    finite.sample.semi.oracle <- rbind(finite.sample.semi.oracle, new.row)
+    
+    
+    # m >> n
+    simulation <- toy.example(n = neff, m = 10*neff, output.oracles = T, noisier.var.X1 = F)
+    ipsw <- ipsw.univariate.and.categorical.X(dataframe = simulation, oracle.e = T, oracle.pt = F, oracle.pr = F)
+    method = "ipsw - m >> n"
+    
+    new.row <- data.frame("estimate" = ipsw,
+                          "method" = method,
+                          "n" = neff,
+                          "m" = 10*neff)
+    
+    finite.sample.semi.oracle <- rbind(finite.sample.semi.oracle, new.row)
+    
+    
+    # n >> m
+    simulation <- toy.example(n = neff, m = neff/10, output.oracles = T, noisier.var.X1 = F)
+    ipsw <- ipsw.univariate.and.categorical.X(dataframe = simulation, oracle.e = T, oracle.pt = F, oracle.pr = F)
+    method = "ipsw - n >> m"
+    
+    new.row <- data.frame("estimate" = ipsw,
+                          "method" = method,
+                          "n" = neff,
+                          "m" = neff/10)
+    
+    finite.sample.semi.oracle <- rbind(finite.sample.semi.oracle, new.row)
+    
+  }
+}
+
+
+write.csv(finite.sample.semi.oracle, "./results/finite.sample.semi.synthetic.csv", row.names = TRUE)
